47854c17b6e035696452078a9393343f8a89bd89
Elvis-92 <elvis.ahissou@epitech.eu>
2025-10-17 10:55:22 +0100

    FEAT: creation and initialization of the project

---
+++ evaluation_frontend/src/api/api.js
@@ -0,0 +1,28 @@
+
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: 'https://127.0.0.1:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    console.log('Requête interceptée', config);
+    return config;
+  },
+  (error) => {
+
+    console.error('Erreur d\'interception de requête:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
